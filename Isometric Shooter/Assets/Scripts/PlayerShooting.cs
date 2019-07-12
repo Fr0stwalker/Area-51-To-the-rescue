de10b65fc60dcabf44c2bcd61e4f8b69ebf9239e
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-12 21:22:28 +0200

    Added Shootable Layer Mask, two new scripts for player prefab and all the needed components for player prefab.

---
+++ Isometric Shooter/Assets/Scripts/PlayerShooting.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShooting : MonoBehaviour
+{
+    [SerializeField] private float timeBetweenBullets = 0.15f;
+
+    [SerializeField] private float range = 50f;
+
+    private float _timer;
+    private Ray _shootRay;
+    private RaycastHit _shootHit;
+    private int _shootableMask;
+    private ParticleSystem _gunParticleSystem;
+    private LineRenderer _gunLineRenderer;
+    private AudioSource _gunAudioSource;
+    private Light _gunLight;
+    private float _effectsDisplayTime=0.2f;
+
+    void Awake()
+    {
+        _shootableMask = LayerMask.GetMask("Shootable");
+
+        _gunParticleSystem = GetComponent<ParticleSystem>();
+        _gunLineRenderer = GetComponent<LineRenderer>();
+        _gunAudioSource = GetComponent<AudioSource>();
+        _gunLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        _timer += Time.deltaTime;
+        if (_timer >= timeBetweenBullets * _effectsDisplayTime)
+        {
+            DisableEffects();
+        }
+    }
+
+    private void DisableEffects()
+    {
+        _gunLineRenderer.enabled = false;
+        _gunLight.enabled = false;
+    }
+
+    private void Shoot()
+    {
+
+    }
+}
