f33ebdf3fbc81634e1019cfbf4ddc0385d18f904
Frostwalker53 <l.witkowski96@gmail.com>
2019-07-12 22:22:21 +0200

    Proste AI przeciwników, nie wykrywa kolizji, idzie prosto w kierunku gracza, znajduje pozycje gracza, oblicza odległość od gracza. Tymczasowo znika przy odległości 1

---
+++ Isometric Shooter/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    [SerializeField] float MovementSpeed = 5f;
+
+    Vector3 playerPosition;
+    float distanceFromEnemyToPlayer;
+    void Start() {
+
+    }
+
+    void Update() {
+        FindPlayerCharacter();
+        DistanceToPlayer();
+        MoveTowardsPlayerPosition();
+        AttackPlayer();
+    }
+
+
+    private void FindPlayerCharacter() {
+        GameObject isPlayerAlive = GameObject.Find("Player");
+        if (isPlayerAlive) {
+            playerPosition = isPlayerAlive.transform.position;
+        }
+    }
+    private void DistanceToPlayer() {
+        distanceFromEnemyToPlayer = Vector3.Distance(
+            GameObject.FindGameObjectWithTag("Player").transform.position,
+            transform.position
+            );
+    }
+    private void MoveTowardsPlayerPosition() {
+        float moveToPlayerSpeed = MovementSpeed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, playerPosition, moveToPlayerSpeed);
+    }
+
+    // TODO: Attack player, currently enemy dissapear in 1 radius
+    private void AttackPlayer() {
+        if(distanceFromEnemyToPlayer <= 1) {
+            Destroy(gameObject);
+        }
+    }
+
+}
