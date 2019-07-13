6d3a248a09c3f5b00e24359613d0beecb1add7de
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-13 19:29:42 +0200

    Started work on spawner and added score

---
+++ Isometric Shooter/Assets/Scripts/EnemyDeath.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDeath : MonoBehaviour
+{
+    private ScoreManagement _scoreManager;
+    private void Awake()
+    {
+        _scoreManager = FindObjectOfType<ScoreManagement>();
+    }
+
+    void OnDestroy()
+    {
+        _scoreManager.CurrentScore += 50;
+    }
+}
---
+++ Isometric Shooter/Assets/Scripts/ScoreManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManagement : MonoBehaviour
+{
+    [SerializeField] private Text scoreText;
+    private int _currentScore;
+    public int CurrentScore
+    {
+        get => _currentScore;
+        set
+        {
+            _currentScore = value;
+            scoreText.text = value.ToString();
+        }
+    }
+}
