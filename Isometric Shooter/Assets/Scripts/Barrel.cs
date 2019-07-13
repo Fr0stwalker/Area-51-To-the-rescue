2def7a3b89e21b6cfd801fb05558e7b43940a80f
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-13 12:37:29 +0200

    Added Barrel Respawn method

--- Isometric Shooter/Assets/Scripts/Barrel.cs
+++ Isometric Shooter/Assets/Scripts/Barrel.cs
@@ -1,22 +1,39 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Threading;
 using UnityEngine;
 
 public class Barrel : MonoBehaviour
 {
     [SerializeField] AudioClip explosionClip;
-
+    [SerializeField] private float respawnTimer=60f;
     private AudioSource _barrelAudioSource;
     private ParticleSystem _explosionParticleSystem;
     private CapsuleCollider _capsuleCollider;
     private bool _isDead;
+    private float _respawnTimer;
+    
 
     private void Awake()
     {
         _barrelAudioSource = GetComponent<AudioSource>();
         _explosionParticleSystem = GetComponent<ParticleSystem>();
         _capsuleCollider = GetComponent<CapsuleCollider>();
+    }
+
+    private void Update()
+    {
+        if (_isDead)
+        {
+            _respawnTimer += Time.deltaTime;
+            //Debug.Log(_respawnTimer);
+        }
+
+        if (respawnTimer <= _respawnTimer)
+        {
+            Respawn();
+        }
     }
 
     public void Death()
@@ -30,6 +47,10 @@ public class Barrel : MonoBehaviour
 
     private void Respawn()
     {
-
+        Debug.Log("Respawn");
+        _capsuleCollider.isTrigger = false;
+        gameObject.transform.GetChild(0).gameObject.SetActive(true);
+        _isDead = false;
+        _respawnTimer = 0f;
     }
 }
