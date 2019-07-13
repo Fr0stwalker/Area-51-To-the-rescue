330713c755bf1b21719f0886cc287acaf24a94d1
Ikuti <m.j.olkiewicz@gmail.com>
2019-07-13 20:14:16 +0200

    Added working Spawner

--- Isometric Shooter/Assets/Scripts/Spawner.cs
+++ Isometric Shooter/Assets/Scripts/Spawner.cs
@@ -1,18 +1,31 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float spawnTimer=10f;
+    [SerializeField] private GameObject enemy;
+    private float timer;
+
+    private void Start()
     {
-        
+        timer = spawnTimer;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+        if (timer >= spawnTimer)
+        {
+            Spawn();
+        }
+    }
+
+    void Spawn()
+    {
+        timer = 0f;
+        Instantiate(enemy,gameObject.transform.position,Quaternion.identity);
     }
 }
